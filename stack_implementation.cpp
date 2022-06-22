72c919cd4ccc3cb3604963e9e483ae97ad898643
nursyahjaya <nursyahjaya@gmail.com>
2022-06-22 12:57:14 +0700

    update

--- stack_implementation.cpp
+++ stack_implementation.cpp
@@ -4,9 +4,34 @@
 #include "custom_stack.h"
 using namespace std;
 
+// not yet implement
+void balancedBrackets(string s){
+
+}
+
+//not yet implement
+void infixtopostfix(string s){
+
+}
+
+void reverseString(string s){
+  string res = "";
+  stack<char> st = stack<char>();
+  int length = s.length();
+
+  for(int i=0; i < length; i++) st.push(s[i]);
+
+  for(int i=0; i < length; i++){
+    res += st.peek();
+    st.pop();
+  }
+
+
+  cout << res << "\n";
+}
 
 int main(){
-  
+  balancedBrackets("(nkas)as{as}");
 
   return 0;
 }
